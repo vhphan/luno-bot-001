5dee8a5ee6037e887be26040adc8168360a0730d
VH Phan <phanveehuen@gmail.com>
2021-11-13 16:45:52 +0800

    Initial commit

--- sandbox.py
+++ sandbox.py
@@ -8,7 +8,9 @@ LUNO_SECRET_KEY = os.getenv('LUNO_SECRET_KEY')
 
 c = Client(api_key_id=LUNO_API_KEY, api_key_secret=LUNO_SECRET_KEY)
 try:
-    res = c.get_ticker(pair='XBTMYR')
-    print(res)
+    ticker = c.get_ticker(pair='XBTMYR')
+    print(ticker)
+    balances = c.get_balances(assets=['MYR', 'ETH'])
+    print(balances)
 except Exception as e:
     print(e)
